24ed4cc21d076898d267f2310feb73fb31246cfc
dgonz66 <dgonz66@bancodebogota.com.co>
2024-09-13 12:16:46 -0500

    shfjhd

--- src/app/bank-virtual/simulator/simulator.component.ts
+++ src/app/bank-virtual/simulator/simulator.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulatorComponent implements OnInit {
   mostrar = false;
+  dias = [{"id": 0,"text": "30","value": "opción 1"},{"id": 1,"text": "60","value": "opción 2"},{"id": 2,"text": "90","value": "opción 3"},{"id": 3,"text": "120","value": "opción 4"},{"id": 4,"text": "Otro","value": "opción 5"}];
+  opcion = [];
+  diasMeses = 'dias';
 
+      meses = [{"id": 0,"text": "3","value": "opción 1"},{"id": 1,"text": "6","value": "opción 2"},{"id": 2,"text": "9","value": "opción 3"},{"id": 3,"text": "12","value": "opción 4"},{"id": 4,"text": "Otro","value": "opción 5"}];
 
   constructor() { }
 
@@ -16,6 +20,17 @@ export class SimulatorComponent implements OnInit {
 
 
   click(evento: any){
-    console.log(evento);
+    console.log(evento.detail.desc);
+    this.mostrar=true;
+    if ( evento.detail.desc === 'Días'){
+this.diasMeses= 'dias'
+      // @ts-ignore
+      this.opcion = this.dias;
+    }else{
+      this.diasMeses= 'meses'
+      // @ts-ignore
+      this.opcion = this.meses;
+    }
+   ;
   }
 }
