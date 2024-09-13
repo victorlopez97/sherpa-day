3131e55e269b0b2bd5262dd39d43b0021e57adab
vlopez5-bdb <vlopez5@bancodebogota.com.co>
2024-09-13 11:44:49 -0500

    Se suben cambiso de routing y otros cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {DashboardComponent} from "./bank-virtual/dashboard/dashboard.component";
 
 const routes: Routes = [
   {
-    path: '', redirectTo: "test", pathMatch: 'full'
+    path: '', redirectTo: "dashboard", pathMatch: 'full'
   },
   {
     path: 'dashboard',
