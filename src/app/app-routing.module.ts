8838f133da437eb7a085e99435a8f43a3ed5ed19
vlopez5-bdb <vlopez5@bancodebogota.com.co>
2024-09-13 11:45:56 -0500

    ruta nueva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./bank-virtual/dashboard/dashboard.component";
+import {SimulatorComponent} from "./bank-virtual/simulator/simulator.component";
 
 const routes: Routes = [
   {
@@ -9,6 +10,10 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+  },
+  {
+    path: 'simulator',
+    component: SimulatorComponent,
   },
 ];
 
